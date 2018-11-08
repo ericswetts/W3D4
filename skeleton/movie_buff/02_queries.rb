2516153d0b0676ee14f56d23af2ac38d060a1cdb
ericswetts <eric.swetts@gmail.com>
2018-11-08 15:19:32 -0800

    Write all queries for movie buffs phase two

--- skeleton/movie_buff/02_queries.rb
+++ skeleton/movie_buff/02_queries.rb
@@ -9,13 +9,21 @@ end
 
 def bad_years
   # List the years in which a movie with a rating above 8 was not released.
+  Movie
+    .group(:yr)
+    .having("MAX(score) <= 8")
+    .pluck(:yr)
 
 end
 
 def cast_list(title)
   # List all the actors for a particular movie, given the title.
   # Sort the results by starring order (ord). Show the actor id and name.
-
+  Actor
+    .joins(:movies)
+    .select(:id, :name)
+    .where(movies: {title: title})
+    .order("castings.ord ASC")
 end
 
 def vanity_projects
@@ -24,11 +32,21 @@ def vanity_projects
   # Show the movie id and title and director's name.
 
   # Note: Directors appear in the 'actors' table.
+  Movie
+    .joins(:actors)
+    .where("ord = 1 AND director_id = actors.id")
+    .select(:id, :title, "actors.name")
 
 end
 
 def most_supportive
   # Find the two actors with the largest number of non-starring roles.
   # Show each actor's id, name and number of supporting roles.
-
+  Actor
+    .group(:id)
+    .order("COUNT(*) DESC")
+    .joins(:castings)
+    .where("castings.ord > 1")
+    .select(:id, :name, "COUNT(*) AS roles")
+    .limit(2)
 end
